8087c40150440fb55db5fc12cae3b3334df513f8
Joksa132 <nikolajoksimovic419@gmail.com>
2025-11-15 16:04:12 +0100

    add vercel analytics component

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import { Toaster } from "./components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next";
 
 const queryClient = new QueryClient({});
 
@@ -21,6 +22,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <Toaster />
+      <Analytics />
     </QueryClientProvider>
   </StrictMode>
 );
